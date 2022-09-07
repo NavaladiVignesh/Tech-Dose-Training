1d255887296ca165c893e4ce442089b89a370027
VIgneh290602 <65125144+VIgneh290602@users.noreply.github.com>
2022-09-07 22:16:49 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -1,25 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int Swap(int *a,int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-}
-
-int main(){
-  int n;
-  scanf("%d",&n);
-  int arr[n];
-  for(int i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-  }
-  for(int i=n-1;i>0;i--){
-    for(int j=0;i<n;j++){
-      if(arr[j]>arr[j+1]){
-        Swap(arr[j],arr[j+1]);
+void bubbleSort(int array[], int size) {
+  for (int step = 0; step < size - 1; ++step) {
+      for (int i = 0; i < size - step - 1; ++i) {
+          if (array[i] > array[i + 1]) {
+          int temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+      }
     }
   }
-  for(int i=0;i<n;i++){
-    printf("%d ",arr[i]);
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
   }
+  printf("\n");
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  int size = sizeof(data) / sizeof(data[0]);
+  bubbleSort(data, size);
+  printf("Sorted Array in Ascending Order:\n");
+  printArray(data, size);
 }

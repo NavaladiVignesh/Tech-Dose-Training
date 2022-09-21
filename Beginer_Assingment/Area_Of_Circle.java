9e4437e68f6fac04ca545b26175facb8f278c74d
VIgneh290602 <65125144+VIgneh290602@users.noreply.github.com>
2022-09-21 18:57:37 +0530

    Delete Beginer_Assingment directory

--- Beginer_Assingment/Area_Of_Circle.java
+++
@@ -1,7 +0,0 @@
-public class Area_Of_Circle {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int r = kb.nextInt();
-        System.out.println("The radius of circle:" + 3.14 * (r * r));
-    }
-}
